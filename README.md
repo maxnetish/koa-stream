94b4b706486c9c913b4a54c48320553546f712af
Maksim Gordeev <Maksim.Gordeev@artezio.com>
2019-08-05 18:45:34 +0400

    Update dependencies, add Koa2 middleware factory, corrected tests

--- README.md
+++ README.md
@@ -1,46 +1,29 @@
-# koa-stream [![Build Status](https://travis-ci.org/claudetech/koa-stream.svg?branch=master)](https://travis-ci.org/claudetech/koa-stream) [![Coverage Status](https://coveralls.io/repos/claudetech/koa-stream/badge.svg)](https://coveralls.io/r/claudetech/koa-stream)
+# Fork of koa-stream
+Bind with Koa2 
 
 Helper to stream files and buffers with range requests using koa.
 This can be used with `video` tags, and other resource using the `Range` header.
 
 The implementation follows [RFC 7233](https://tools.ietf.org/html/rfc7233).
+With exception of __If-range__ header support.
 
-## Installation
-
-```sh
-$ npm install koa-stream
+##Usage
 ```
+app.use(stream.middlewareFactory({
+        resolveFilepath: ctx => filepath,
+        root: '/path/to',
+        allowDownload: true
+}));
 
-## Usage
-
-### Stream Files
-
-```javascript
-var stream = require('koa-stream');
-var path   = require('path');
-var app    = require('koa');
-
-app.use(function *() {
-    yield stream.file(this, 'my-video.mp4', {root: path.join(__dirname, '/public')});
-});
+app.use(stream.middlewareFactory({
+        resolveBuffer: ctx => ({buffer: testBuffer, contentType: contentType}),
+        allowDownload: true
+}));
 ```
-
-### Stream Buffers
-
-```javascript
-var stream = require('koa-stream');
-var path   = require('path');
-var app    = require('koa');
-
-app.use(function *() {
-    stream.buffer(this, new Buffer([1,2,3]), 'image/png', {allowDownload: true});
-});
-```
-
-See [sample/index.js](./sample/index.js) for a working example.
 
 ### Options
-
+* `resolveFilepath`: to get filepath to download
+* `resolveBuffer`: to get Buffer to stream
 * `root`: the directory from which file paths will be resolved
 * `allowDownload`: allow to return the file instead of streaming it if not `Range` header is provided
 
--- index.js
+++ index.js
@@ -1,151 +1,181 @@
 'use strict';
 
-const assert      = require('assert');
+const assert = require('assert');
 const resolvePath = require('resolve-path');
-const path        = require('path');
-const mime        = require('mime');
-const fs          = require('mz/fs');
+const path = require('path');
+const mime = require('mime');
+const fs = require('mz/fs');
 
 const parseRange = function (range, totalLength) {
-  if (typeof range === 'undefined' || range === null || range.length === 0) {
-    return null;
-  }
+    if (typeof range === 'undefined' || range === null || range.length === 0) {
+        return null;
+    }
 
-  let array = range.split(/bytes=([0-9]*)-([0-9]*)/);
-  let result = {
-    start: parseInt(array[1]),
-    end: parseInt(array[2])
-  };
+    let array = range.split(/bytes=([0-9]*)-([0-9]*)/);
+    let result = {
+        start: parseInt(array[1]),
+        end: parseInt(array[2])
+    };
 
-  if (isNaN(result.end) || result.end < 0) {
-    result.end = totalLength - 1;
-  }
+    if (isNaN(result.end) || result.end < 0) {
+        result.end = totalLength - 1;
+    }
 
-  if (isNaN(result.start) || result.start < 0) {
-    result.start = 0;
-  }
+    if (isNaN(result.start) || result.start < 0) {
+        result.start = 0;
+    }
 
-  result.totalLength = totalLength;
+    result.totalLength = totalLength;
 
-  return result;
+    return result;
 };
 
 const endRequest = function (ctx, size) {
-  ctx.set('Content-Range', 'bytes */' + size);
-  ctx.body = null;
-  ctx.status = 416;
+    ctx.set('Content-Range', 'bytes */' + size);
+    ctx.body = null;
+    ctx.status = 416;
 };
 
 const sendFile = function (ctx, filepath, size) {
-  ctx.set('Content-Type', mime.lookup(filepath));
-  ctx.set('Content-Length', size);
-  ctx.set('Accept-Ranges', 'bytes');
-  ctx.body = fs.createReadStream(filepath);
+    ctx.set('Content-Type', mime.getType(filepath));
+    ctx.set('Content-Length', size);
+    ctx.set('Accept-Ranges', 'bytes');
+    ctx.body = fs.createReadStream(filepath);
 };
 
 const sendBufferAtOnce = function (ctx, buffer, type) {
-  ctx.set('Content-Type', type);
-  ctx.set('Content-Length', buffer.length);
-  ctx.body = buffer;
+    ctx.set('Content-Type', type);
+    ctx.set('Content-Length', buffer.length);
+    console.log(`rite to body: ${buffer}`);
+    ctx.body = buffer;
 };
 
 const streamRange = function (ctx, body, range, contentType) {
-  ctx.set('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + range.totalLength);
-  ctx.set('Content-Length', range.end - range.start + 1);
-  ctx.set('Content-Type', contentType);
-  ctx.set('Accept-Ranges', 'bytes');
-  ctx.set('Cache-Control', 'no-cache');
-  ctx.status = 206;
-  ctx.body = body;
+    ctx.set('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + range.totalLength);
+    ctx.set('Content-Length', range.end - range.start + 1);
+    ctx.set('Content-Type', contentType);
+    ctx.set('Accept-Ranges', 'bytes');
+    ctx.set('Cache-Control', 'no-cache');
+    ctx.status = 206;
+    ctx.body = body;
 };
 
 const handleFileStream = function (ctx, range, filepath) {
-  let stream = fs.createReadStream(filepath, {start: range.start, end: range.end});
-  let contentType = mime.lookup(filepath);
-  streamRange(ctx, stream, range, contentType);
+    let stream = fs.createReadStream(filepath, {start: range.start, end: range.end});
+    let contentType = mime.getType(filepath);
+    streamRange(ctx, stream, range, contentType);
 };
 
-const getFileStat = function *(filepath) {
-  try {
-    let stats = yield fs.stat(filepath);
-    if (stats.isDirectory()) {
-      return false;
+async function getFileStat(filepath) {
+    try {
+        const stats = await fs.stat(filepath);
+        if (stats.isDirectory()) {
+            return false;
+        }
+        return stats;
+    } catch (err) {
+        const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
+        if (notfound.indexOf(err.code) !== -1) {
+            return false;
+        }
+        err.status = 500;
+        throw err;
     }
-    return stats;
-  } catch (err) {
-    let notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
-    if (notfound.indexOf(err.code) !== -1) {
-      return false;
+}
+
+async function handleRequest(ctx, filepath, options) {
+    const stat = await getFileStat(filepath);
+    if (!stat) {
+        return;
     }
-    err.status = 500;
-    throw err;
-  }
-};
 
-const handleRequest = function *(ctx, filepath, options) {
-  let stat = yield getFileStat(filepath);
-  if (!stat) {
-    return;
-  }
+    let range = parseRange(ctx.headers.range, stat.size);
 
-  let range = parseRange(ctx.headers.range, stat.size);
+    if (range === null) {
+        return options.allowDownload ? sendFile(ctx, filepath, stat.size) : null;
+    }
 
-  if (range === null) {
-    return options.allowDownload ? sendFile(ctx, filepath, stat.size) : null;
-  }
+    if (range.start >= stat.size || range.end >= stat.size) {
+        return endRequest(ctx, stat.size);
+    }
 
-  if (range.start >= stat.size || range.end >= stat.size) {
-    return endRequest(ctx, stat.size);
-  }
-
-  handleFileStream(ctx, range, filepath, stat);
-};
+    handleFileStream(ctx, range, filepath, stat);
+}
 
 const handleBuffer = function (ctx, buffer, contentType, options) {
-  let range = parseRange(ctx.headers.range, buffer.length);
+    let range = parseRange(ctx.headers.range, buffer.length);
 
-  if (range === null) {
-    return options.allowDownload ? sendBufferAtOnce(ctx, buffer, contentType) : null;
-  }
+    if (range === null) {
+        return options.allowDownload ? sendBufferAtOnce(ctx, buffer, contentType) : null;
+    }
 
-  if (range.start >= buffer.length || range.end >= buffer.length) {
-    return endRequest(ctx, buffer.length);
-  }
+    if (range.start >= buffer.length || range.end >= buffer.length) {
+        return endRequest(ctx, buffer.length);
+    }
 
-  let bufferSlice = buffer.slice(range.start, range.end);
-  streamRange(ctx, bufferSlice, range, contentType);
+    let bufferSlice = buffer.slice(range.start, range.end);
+    streamRange(ctx, bufferSlice, range, contentType);
 };
 
 const decode = function (filepath) {
-  try {
-    return decodeURIComponent(filepath);
-  } catch (err) {
-    return -1;
-  }
+    // return filepath;
+    try {
+        return decodeURIComponent(filepath);
+    } catch (err) {
+        console.log(err);
+        return -1;
+    }
 };
 
 const streamFile = function (ctx, filepath, options) {
-  assert(ctx, 'koa context required');
-  assert(filepath, 'filepath required');
-  options = options || {};
-  let root = options.root ? path.normalize(path.resolve(options.root)) : '';
-  filepath = filepath[0] === '/' ? filepath.slice(1) : filepath;
-  filepath = decode(filepath);
-  if (filepath === -1) {
-    return ctx.throw('failed to decode', 400);
-  }
-  filepath = resolvePath(root, filepath);
-  return handleRequest(ctx, filepath, options);
+    assert(ctx, 'koa context required');
+    assert(filepath, 'filepath required');
+    options = options || {};
+    let root = options.root ? path.normalize(path.resolve(options.root)) : '';
+    filepath = filepath[0] === '/' ? filepath.slice(1) : filepath;
+    filepath = decode(filepath);
+    if (filepath === -1) {
+        return ctx.throw(400, 'failed to decode');
+    }
+    filepath = resolvePath(root, filepath);
+    return handleRequest(ctx, filepath, options);
 };
 
 const streamBuffer = function (ctx, buffer, contentType, options) {
-  assert(ctx, 'koa context required');
-  assert(buffer instanceof Buffer, 'buffer required');
-  options = options || {};
-  return handleBuffer(ctx, buffer, contentType, options);
+    assert(ctx, 'koa context required');
+    assert(buffer instanceof Buffer, 'buffer required');
+    options = options || {};
+    return handleBuffer(ctx, buffer, contentType, options);
 };
 
+/**
+ *
+ * resolveFilepath or resolveBuffer required
+ * @param options {resolveFilepath(ctx): Promise<string>, resolveBuffer(ctx): Promise<{buffer:Buffer, contentType:string}>, root: string, allowDownload: boolean } options
+ * @returns {Function}
+ */
+function middlewareFactory(options) {
+    options = options || {};
+    return async function (ctx) {
+        if (options.resolveFilepath) {
+            const filepath = await options.resolveFilepath(ctx);
+            await streamFile(ctx, filepath, {root: options.root, allowDownload: options.allowDownload});
+            return;
+        }
+        if (options.resolveBuffer) {
+            const {buffer, contentType} = await options.resolveBuffer(ctx);
+            console.log(`GOT buffer: ${buffer}`);
+            await streamBuffer(ctx, buffer, contentType, {root: options.root, allowDownload: options.allowDownload});
+            console.log(`Response biy: ${ctx.body}`);
+            return;
+        }
+
+        ctx.throw(500, 'Cannot resolve filepath or buffer from request');
+    };
+}
+
 module.exports = {
-	file: streamFile,
-	buffer: streamBuffer
+    file: streamFile,
+    buffer: streamBuffer,
+    middlewareFactory,
 };
